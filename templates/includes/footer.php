a974c255d8af5d54582b3e82f8d01afabbf48d19
Marine Romeu <mromeu@stagiaire-humanbooster.com>
2021-12-13 16:49:07 +0100

    styling

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -29,14 +29,7 @@
     </div>
     <div class="col-md-2 my-2" id="contact">
         <p>Une question ?</p>
-        <a href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right"
-                viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-            </svg>
-            Notre FAQ
-        </a><br>
+        <div>
         <a href="{{ path('contact')}}">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right"
                 viewBox="0 0 16 16">
@@ -44,8 +37,10 @@
                     d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
             </svg>
             Contactez-nous
-        </a><br>
-        <a href="{{ path('sav') }}">
+        </a>
+        </div>
+        <div>
+            <a href="{{ path('sav') }}">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right"
                 viewBox="0 0 16 16">
                 <path fill-rule="evenodd"
@@ -53,6 +48,8 @@
             </svg>
             Service après vente
         </a>
+        </div>
+        
     </div>
     <div class="row d-inline text-center">
         <a href="#">
